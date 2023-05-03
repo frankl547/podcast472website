ec8985520eb6c63f2853e4ce500baa5dad0aa68b
Frank L <55472735+frankl547@users.noreply.github.com>
2023-05-03 17:46:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const rssUrl = "https://rss.art19.com/how-i-built-this"; // Replace with your Anchor RSS URL
+
+    fetch(rssUrl)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const episodes = data.querySelectorAll("item");
+            const episodeContainer = document.getElementById("episode-container");
+
+            episodes.forEach(episode => {
+                const title = episode.querySelector("title").textContent;
+                const audioUrl = episode.querySelector("enclosure").getAttribute("url");
+
+                const episodeDiv = document.createElement("div");
+                episodeDiv.classList.add("episode");
+
+                const episodeTitle = document.createElement("h3");
+                episodeTitle.textContent = title;
+                episodeDiv.appendChild(episodeTitle);
+
+                const audioElement = document.createElement("audio");
+                audioElement.controls = true;
+
+                const audioSource = document.createElement("source");
+                audioSource.src = audioUrl;
+                audioSource.type = "audio/mpeg";
+                audioElement.appendChild(audioSource);
+                episodeDiv.appendChild(audioElement);
+
+                episodeContainer.appendChild(episodeDiv);
+            });
+        })
+        .catch(err => {
+            console.error("Error fetching RSS feed:", err);
+        });
+});
+
+
